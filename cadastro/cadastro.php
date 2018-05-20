9d4e99800be3fd211fc99a34790ee7b3baaa871f
Lucas <lucaspaulom@hotmail.com>
2018-05-20 20:26:02 -0300

    Estilização da pagina de login e cadastro.

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -1,19 +1,66 @@
 <?php
-  session_start();
-  require "../INCs/controleLogin.inc";
+    session_start();
+    require "../INCs/controleLogin.inc";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="../css/style.css"/>
 
-<form action="insereUsuario.php"  method="post">
-  User:
-  <input type="text" name="user" required>
-  Email:
-  <input type="email" name="email" required>
-  Senha:
-  <input type="text" name="senha" required>
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
 
-  <input type="submit" value="Submit">
-</form>
-
-<?php
-  require "../utils/errors.inc";
-?>
+<body>
+	<div class="container">
+		
+		<div class="row card-panel z-depth-5 center-align">
+			<div class="row">
+				<a class="btn-floating btn-large pulse" href="../login/login.php"><i class="material-icons">cloud</i></a>
+			</div>
+			<form class="col s12" action="insereUsuario.php"  method="post">
+				<div class="row">
+					<div class="input-field">
+						<input placeholder="Nome" id="first_name" type="text" class="validate" name="user" required>
+						<label for="first_name">Usuário</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field">
+						<input id="email" type="email" class="validate" name="email" required>
+						<label for="email">Email</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field">
+						<input id="password" type="password" class="validate" name="senha" required>
+						<label for="password">Senha</label>
+					</div>
+				</div>
+				<div class="row">
+					<button class="btn waves-effect waves-light" type="submit">Submit
+						<i class="material-icons right">send</i>
+					</button>		
+				</div>	
+			</form>
+			<div class="row">
+				<a href="../cadastro/cadastro.php">
+					<button class="btn waves-effect waves-light" type="submit">Cadastrar
+						<i class="material-icons right"></i>
+					</button>	
+				</a>
+			</div>
+			<?php
+				require "../utils/errors.inc";
+			?>
+		</div>
+	</div>
+	<!--JavaScript at end of body for optimized loading-->
+	<script type="text/javascript" src="../js/materialize.min.js"></script>
+	<script type="text/javascript" src="../js/script.js"></script>
+</body>
+</html>
--- login/login.php
+++ login/login.php
@@ -2,16 +2,59 @@
     session_start();
     require "../INCs/controleLogin.inc";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="../css/style.css"/>
 
-<form action="confereLogin.php"  method="post">
-	User:
-	<input type="text" name="user" required>
-	Senha:
-	<input type="text" name="senha" required>
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
 
-	<input type="submit" value="Submit">
-</form>
-<br><a href="../cadastro/cadastro.php"> Cadastrar</a>
-<?php
-  	require "../utils/errors.inc";
-?>
+<body>
+	<div class="container">
+		
+		<div class="row card-panel z-depth-5 center-align">
+			<div class="row">
+				<a class="btn-floating btn-large pulse"><i class="material-icons">cloud</i></a>
+			</div>
+			<form class="col s12" action="confereLogin.php"  method="post">
+				<div class="row">
+					<div class="input-field">
+						<input placeholder="Nome" id="first_name" type="text" class="validate" name="user" required>
+						<label for="first_name">Usuário</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field">
+						<input id="password" type="password" class="validate" name="senha" required>
+						<label for="password">Senha</label>
+					</div>
+				</div>
+				<div class="row">
+					<button class="btn waves-effect waves-light" type="submit">Submit
+						<i class="material-icons right">send</i>
+					</button>
+				</div>					
+			</form>
+			<div class="row">
+				<a href="../cadastro/cadastro.php">
+					<button class="btn waves-effect waves-light" type="submit">Cadastrar
+						<i class="material-icons right"></i>
+					</button>	
+				</a>
+			</div>
+			<?php
+				require "../utils/errors.inc";
+			?>
+		</div>
+	</div>
+	<!--JavaScript at end of body for optimized loading-->
+	<script type="text/javascript" src="../js/materialize.min.js"></script>
+	<script type="text/javascript" src="../js/script.js"></script>
+</body>
+</html>
